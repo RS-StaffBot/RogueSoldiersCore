1694710b87e44989a1d58e8f5031de0e653a7e0f
RubbaDuckie <tyl3r.l3clair@gmail.com>
2026-07-22 22:02:50 -0400

    Milestone 2 - Discord provider online

--- src/providers/discord/DiscordProvider.js
+++ src/providers/discord/DiscordProvider.js
@@ -1,9 +1,71 @@
 const BaseProvider = require("../core/BaseProvider");
+const Logger = require("../../core/Logger");
+
+const { Client, GatewayIntentBits } = require("discord.js");
 
 class DiscordProvider extends BaseProvider {
+
     constructor() {
+
         super("Discord");
+
+        this.client = null;
+
     }
+
+    start() {
+
+        super.start();
+
+        this.client = new Client({
+
+            intents: [
+                GatewayIntentBits.Guilds
+            ]
+
+        });
+
+        this.client.once("clientReady", () => {
+
+            Logger.info("");
+            Logger.info("========================================");
+            Logger.info("Discord Connected");
+            Logger.info("========================================");
+            Logger.info(`Logged in as ${this.client.user.tag}`);
+            Logger.info(`Connected to ${this.client.guilds.cache.size} server(s).`);
+            Logger.info("");
+
+        });
+
+        const token = process.env.DISCORD_TOKEN;
+
+        if (!token) {
+
+            Logger.error("DISCORD_TOKEN is missing from the .env file.");
+
+            return;
+
+        }
+
+        this.client.login(token).catch(error => {
+
+            Logger.error("Failed to log into Discord.");
+            Logger.error(error.message);
+
+        });
+
+    }
+
+    stop() {
+
+        if (this.client) {
+            this.client.destroy();
+        }
+
+        super.stop();
+
+    }
+
 }
 
 module.exports = DiscordProvider;
