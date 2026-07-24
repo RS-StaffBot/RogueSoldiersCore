87c1bd64a83d72f1dfcda7397f5b653a9b79a312
RubbaDuckie <tyl3r.l3clair@gmail.com>
2026-07-24 19:10:48 -0400

    v0.3.1 Phase 1 - Strengthen Discord command registry

--- src/providers/discord/services/CommandRegistry.js
+++ src/providers/discord/services/CommandRegistry.js
@@ -8,7 +8,19 @@ class CommandRegistry {
 
     register(command) {
 
-        this.commands.set(command.name, command);
+        this.validateCommand(command);
+
+        const commandName = command.data.name;
+
+        if (this.commands.has(commandName)) {
+
+            throw new Error(
+                `Discord command '${commandName}' is already registered.`
+            );
+
+        }
+
+        this.commands.set(commandName, command);
 
     }
 
@@ -24,9 +36,72 @@ class CommandRegistry {
 
     }
 
+    getDefinitions() {
+
+        return this.getAll().map(command => {
+
+            return command.data.toJSON();
+
+        });
+
+    }
+
     has(name) {
 
         return this.commands.has(name);
+
+    }
+
+    clear() {
+
+        this.commands.clear();
+
+    }
+
+    validateCommand(command) {
+
+        if (!command || typeof command !== "object") {
+
+            throw new TypeError(
+                "A Discord command instance is required."
+            );
+
+        }
+
+        if (!command.data) {
+
+            throw new TypeError(
+                "Discord commands must provide command data."
+            );
+
+        }
+
+        if (
+            typeof command.data.name !== "string"
+            || command.data.name.length === 0
+        ) {
+
+            throw new TypeError(
+                "Discord commands must provide a valid command name."
+            );
+
+        }
+
+        if (typeof command.data.toJSON !== "function") {
+
+            throw new TypeError(
+                `Discord command '${command.data.name}' must provide serializable command data.`
+            );
+
+        }
+
+        if (typeof command.execute !== "function") {
+
+            throw new TypeError(
+                `Discord command '${command.data.name}' must implement execute().`
+            );
+
+        }
 
     }
 
