42a769b2d725f2ba96ecace9c972d665ab9b3f6f
RubbaDuckie <tyl3r.l3clair@gmail.com>
2026-07-24 23:09:51 -0400

    define moderation action contract

---
+++ src/modules/moderation/ModerationAction.js
@@ -0,0 +1,9 @@
+const ModerationAction = Object.freeze({
+    BAN: "BAN",
+    KICK: "KICK",
+    WARN: "WARN",
+    TIMEOUT: "TIMEOUT",
+    PURGE: "PURGE"
+});
+
+module.exports = ModerationAction;
--- src/modules/moderation/ModerationModule.js
+++ src/modules/moderation/ModerationModule.js
@@ -1,9 +1,22 @@
 const BaseModule = require("../core/BaseModule");
+const ModerationAction = require("./ModerationAction");
 
 class ModerationModule extends BaseModule {
 
     constructor() {
         super("Moderation");
+
+        this.actions = new Set(
+            Object.values(ModerationAction)
+        );
+    }
+
+    supports(action) {
+        return this.actions.has(action);
+    }
+
+    listActions() {
+        return [...this.actions];
     }
 
 }
