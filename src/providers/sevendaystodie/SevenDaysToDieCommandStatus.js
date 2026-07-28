be1ebcc26aa8f2a11f4ed00afb9d1ce9fb37bb48
Rubba-Duckie <tyl3r.l3clair@gmail.com>
2026-07-27 22:20:13 -0400

    Add 7DTD command status contract

---
+++ src/providers/sevendaystodie/SevenDaysToDieCommandStatus.js
@@ -0,0 +1,8 @@
+const SevenDaysToDieCommandStatus = Object.freeze({
+    SUCCESS: "SUCCESS",
+    ERROR: "ERROR",
+    TIMEOUT: "TIMEOUT",
+    DISCONNECTED: "DISCONNECTED"
+});
+
+module.exports = SevenDaysToDieCommandStatus;
