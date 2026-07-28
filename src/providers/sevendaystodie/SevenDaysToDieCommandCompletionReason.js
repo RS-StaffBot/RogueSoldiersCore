09a197eef586b11afd00e329d4f82cca17f2b04e
Rubba-Duckie <tyl3r.l3clair@gmail.com>
2026-07-27 22:20:25 -0400

    Add 7DTD command completion reasons

---
+++ src/providers/sevendaystodie/SevenDaysToDieCommandCompletionReason.js
@@ -0,0 +1,10 @@
+const SevenDaysToDieCommandCompletionReason = Object.freeze({
+    MATCHED_RULE: "MATCHED_RULE",
+    INVALID_COMMAND: "INVALID_COMMAND",
+    INACTIVITY: "INACTIVITY",
+    TIMEOUT: "TIMEOUT",
+    DISCONNECTED: "DISCONNECTED",
+    SIZE_LIMIT: "SIZE_LIMIT"
+});
+
+module.exports = SevenDaysToDieCommandCompletionReason;
