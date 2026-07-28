fe1d92e8c928cce41ff931bee77fbdabab907297
Rubba-Duckie <tyl3r.l3clair@gmail.com>
2026-07-28 19:21:33 -0400

    Update time command definition test for say

--- tests/providers/discord/game-time-command.test.js
+++ tests/providers/discord/game-time-command.test.js
@@ -76,7 +76,7 @@ test("defines the game time subcommand", () => {
 
     assert.deepEqual(
         data.options.map(option => option.name),
-        ["status", "time", "players"]
+        ["status", "time", "players", "say"]
     );
 });
 
