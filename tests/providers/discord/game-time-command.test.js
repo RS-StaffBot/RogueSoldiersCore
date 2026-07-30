6fa3ee9e9a8b0eb760a4083a33ad7f66245b8a93
Rubba-Duckie <tyl3r.l3clair@gmail.com>
2026-07-30 18:23:57 -0400

    Include unban in game time definition coverage

--- tests/providers/discord/game-time-command.test.js
+++ tests/providers/discord/game-time-command.test.js
@@ -76,7 +76,7 @@ test("defines the game time subcommand", () => {
 
     assert.deepEqual(
         data.options.map(option => option.name),
-        ["status", "time", "players", "say", "kick", "ban"]
+        ["status", "time", "players", "say", "kick", "ban", "unban"]
     );
 });
 
